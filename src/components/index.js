390e58ff19dd2052f08ab56bf1f9bbc872b10f7b
Barchakuz <behroze.barcha@gmail.com>
2024-08-18 15:07:59 +0500

    Login Component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,9 @@ import Footer from "./footer/Footer";
 import LogoutBtn from "./header/LogoutBtn";
 import Logo from "./Logo";
 import Container from "./container/Container";
+import Button from "./Button";
+import Input from "./Input";
+import PostCard from "./PostCard";
+import Login from "./Login";
  
-export {Header, Footer, LogoutBtn, Logo, Container}
+export {Header, Footer, LogoutBtn, Logo, Container, Input, PostCard,Button, Login}
